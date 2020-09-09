b32f8302f2c3b0f031c2fa73c8f72c6b4111cf66
Kevin Schreier <kevin.schreier@gus-group.com>
2020-09-09 16:19:41 +0200

    fom_object-oriented_ws2020
    First commit for the first session

---
+++ src/task1/Person.java
@@ -0,0 +1,40 @@
+/**
+ * 
+ */
+package task1;
+
+/**
+ * @author kschreier
+ *
+ */
+public class Person {
+	
+	private String vorname;
+	private String name;
+	
+	/**
+	 * @return the vorname
+	 */
+	public String getVorname() {
+		return vorname;
+	}
+	/**
+	 * @param vorname the vorname to set
+	 */
+	public void setVorname(String vorname) {
+		this.vorname = vorname;
+	}
+	/**
+	 * @return the name
+	 */
+	public String getName() {
+		return name;
+	}
+	/**
+	 * @param name the name to set
+	 */
+	public void setName(String name) {
+		this.name = name;
+	}
+	
+}
---
+++ src/task1/TestPerson.java
@@ -0,0 +1,31 @@
+/**
+ * 
+ */
+package task1;
+
+/**
+ * @author kschreier
+ *
+ */
+public class TestPerson {
+
+	/**
+	 * @param args
+	 */
+	public static void main(String[] args) {
+		Person p1 = new Person();
+		
+		p1.setVorname("Max");
+		p1.setName("Mustermann");
+		
+		Person p2;
+		p2 = new Person();
+		
+		p2.setVorname("Ralf");
+		p2.setName("Meister");
+	
+		System.out.println(p1.getName() + " " + p1.getVorname());
+		System.out.println(p2.getName() + " " + p2.getVorname());
+	}
+
+}
